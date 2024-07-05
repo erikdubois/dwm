12386843f70ae91b023956e628e7a11f1c2f9091
Erik Dubois <erik.dubois@gmail.com>
2024-07-05 13:13:21 +0200

    spaces

--- config.def.custom.h
+++ config.def.custom.h
@@ -145,7 +145,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,    XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,    			XK_q,      quit,           {0} },
 };
 
 /* button definitions */
