abe86db8752288a40d13353b98324f287417f252
Erik Dubois <erik.dubois@gmail.com>
2024-07-05 13:44:59 +0200

    update

--- build-custom-dwm.sh
+++ build-custom-dwm.sh
@@ -2,6 +2,8 @@
 
 sudo rm -r dwm
 sudo cp -v dwm.desktop /usr/share/xsessions/dwm.desktop
+mkdir ~/.config/dwm
+cp -v autostart.sh ~/.config/dwm
 git clone https://git.suckless.org/dwm
 cp -v rebuild.sh dwm
 mkdir dwm/patches
@@ -11,11 +13,13 @@ sh get-patches.sh
 cd ..
 
 #patch < patches/dwm-systray-20230922-9f88553.diff
+#sh rebuild.sh
+#exit 1
+
 patch < patches/dwm-cfacts-vanitygaps-6.4_combo.diff
 patch < patches/dwm-alpha-20230401-348f655.diff
 patch < patches/dwm-autostart-20210120-cb3f58a.diff
 patch < patches/dwm-bartoggle-6.4.diff
-
 
 cp -v config.def.h ../config.def.h
 cd ..
